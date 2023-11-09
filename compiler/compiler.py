b2448b6479c2a2abd540cfdf95fd4afa31cc9d6e
M1dnight <108243683+M1dnight-ofcl@users.noreply.github.com>
2023-11-09 11:08:15 -0500

    changed dirs, added .sb3 compiler

--- compiler/compiler.py
+++ compiler/compiler.py
@@ -1,4 +1,4 @@
-import sys, os, pathlib, shlex
+import sys, os, pathlib, shlex, shutil
 from time import sleep
 
 class Compiler:
@@ -10,6 +10,7 @@ class Compiler:
         self.filePath = sys.argv[1]
         self.file = open(self.filePath, "r")
         self.beginParsingFile(self.file)
+        # self.compileSB3Project()
 
     def beginParsingFile(self, file):
         print("> initiliazing file parse")
@@ -17,21 +18,22 @@ class Compiler:
         self.fileLines = self.fileContents.split('\n')
         # print(self.fileLines)
         i=0
+        self.fileLinesSplit=[]
         for lineContents in self.fileLines:
             lineSplit = shlex.split(lineContents)
-            if not len(lineContents) < 1:
+            if not len(lineSplit) < 1:
                 try:
                     # print(f'+ > line {i+1}: {lineSplit}')
-                    if lineSplit[0][0] == "#": del self.fileLines[i]
-                    else: pass
+                    if lineSplit[0][0] == "#": pass
+                    else: self.fileLinesSplit.append(lineSplit)
                 except Exception as e: del self.fileLines[i]
                     # print(f'+ > line {i+1}: parse failed, reason: {e}')
             else: pass
             i=i+1
-        print(self.fileLines)
+        print(*self.fileLinesSplit, sep='\n')
 
     def runInitialCheck(self):
-        if len(sys.argv)-1 < 1: raise AttributeError("Please specify A file to compile")
+        if len(sys.argv)-1 < 2: raise AttributeError("Please specify A file to compile")
         file = sys.argv[1]
         print(f'> got file arg "{file}"')
         print('> initializing compiler')
@@ -41,5 +43,12 @@ class Compiler:
             raise TypeError(f"File type ({flExt}) invalid")
         print('> checking file existance')
         if not os.path.exists(file):  raise FileNotFoundError(f"File {pathlib.Path(file).absolute()} does not exist")
+
+    def compileSB3Project(self):
+        print("> intializing compile to .sb3")
+        shutil.make_archive(sys.argv[2], 'zip', pathlib.Path(sys.argv[1]).parent.absolute())
+        p=pathlib.Path(pathlib.Path(pathlib.Path(sys.argv[1]).parent.absolute().parts[-1]).parts[-1]+".zip")
+        p.rename(p.with_suffix('.sb3'))
+        print("> successfully compiled")
 
 Compiler()
